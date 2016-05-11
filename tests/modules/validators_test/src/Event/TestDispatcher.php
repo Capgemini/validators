193339aaa05d88889c5cd83341d3bcec5c3cf988
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-11 09:28:01 +0100

    Progress so far

---
+++ tests/modules/validators_test/src/Event/TestDispatcher.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\validators_test\TestDispatcher.
+ */
+
+namespace Drupal\validators_test;
+
+/**
+ * Defines a TestDispatcher, used for testing the event dispatcher.
+ */
+class TestDispatcher {
+
+}
